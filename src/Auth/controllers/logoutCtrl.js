7ab62a71a0e1d06925abe8d1ba8647d5a3236360
joao <joao.brito.ramos@gmail.com>
2015-02-13 17:06:34 +0000

    Register Forms

--- src/Auth/controllers/logoutCtrl.js
+++ src/Auth/controllers/logoutCtrl.js
@@ -3,7 +3,6 @@
 
   angular.module('IpsumFE.Auth').controller('logoutCtrl', function ($scope, authtoken,$rootScope,$state, alert, initSet) {
   
- 
       authtoken.removeToken();
       alert('default', 'Good bye!', 'See you later '+ initSet.email);
       $state.go('home');
--- src/Auth/controllers/registerCtrl.js
+++ src/Auth/controllers/registerCtrl.js
@@ -1,10 +1,20 @@
 'use strict';
 
 
-  angular.module('IpsumFE.Auth').controller('registerCtrl', function ($scope, authtoken,$rootScope,$state, alert) {
+  angular.module('IpsumFE.Auth').controller('registerCtrl', function ($scope, authtoken, alert) {
 
     $scope.stage1=false;
-      
+    $scope.email="";
+    $scope.password="";
+    $scope.password_confirm="";
+    $scope.formValidated=false;
+    $scope.firstname="";
+    $scope.lastname="";
+    $scope.phone="";
+    $scope.address="";
+    $scope.passwordOK=false;
+  
+
     $scope.next = function(){
         $scope.stage1=true;
     };
@@ -13,4 +23,46 @@
         $scope.stage1=false;
     };
 
+    $scope.register = function(){
+        console.log("Registering");
+    };
+    
+    $scope.checkPass = function(){
+        console.log("Checking Password");
+    };
+    
+    $scope.$watch('password', function(newval) {
+        // do something here
+        if (newval!= undefined){
+            if (newval.length > 7 && $scope.email.length>2 && newval == $scope.password_confirm){
+                $scope.formValidated=true;
+            }else{
+                $scope.formValidated=false;
+            }
+        }
+    });
+    $scope.$watch('password_confirm', function(newval) {
+        // do something here
+        if (newval!= undefined){
+            if (newval == $scope.password){
+                $scope.passwordOK=true;
+                if ($scope.email.length > 2 && $scope.password.length>7) $scope.formValidated=true;
+            }else{
+                $scope.passwordOK=false;
+                 $scope.formValidated=false;
+            }
+        }
+    });
+    
+    $scope.$watch('email', function(newval) {
+        // do something here
+        if (newval!= undefined){
+            if (newval.length > 2 && $scope.password.length>7){
+                $scope.formValidated=true;
+            }else{
+                $scope.formValidated=false;
+            }
+        }
+    });  
+      
   });
