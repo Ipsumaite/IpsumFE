4aa9b2722001687d6b6e7ae350d5a04620061bc9
Joao Ramos <joao.brito.ramos@gmail.com>
2015-02-17 00:10:12 +0000

    Create Channel - Form List

--- dist/Auth/services/authtoken.js
+++ dist/Auth/services/authtoken.js
@@ -6,32 +6,35 @@ angular.module('IpsumFE.Auth').factory('authtoken', function ($window) {
     var cachedToken;
     var userToken;
     var dateToken;
-    
+
     // Public API here
-    var authtoken =  {
-      setToken: function (token) {
-        cachedToken=token;
-        storage.setItem(userToken, token);
-        storage.setItem(dateToken, new Date().toISOString());
-      },
-      getToken: function(){
-         if (!cachedToken)
-             cachedToken = storage.getItem(userToken);
-         return cachedToken;
-      },
-      getTokenDate: function(){
-         return storage.getItem(dateToken);
-      },
-      isAuthenticated: function(){
-        return !!authtoken.getToken();           
-      },
-      removeToken: function(){
-        cachedToken = null;
-        storage.removeItem(userToken);
-        storage.removeItem(dateToken);
-      }
+    var authtoken = {
+        setToken: function (token) {
+            cachedToken = token;
+            storage.setItem(userToken, token);
+            storage.setItem(dateToken, new Date().toISOString());
+        },
+        setTokenDate: function () {
+            storage.setItem(dateToken, new Date().toISOString());
+        },
+        getToken: function () {
+            if (!cachedToken)
+                cachedToken = storage.getItem(userToken);
+            return cachedToken;
+        },
+        getTokenDate: function () {
+            return storage.getItem(dateToken);
+        },
+        isAuthenticated: function () {
+            return !!authtoken.getToken();
+        },
+        removeToken: function () {
+            cachedToken = null;
+            storage.removeItem(userToken);
+            storage.removeItem(dateToken);
+        }
     };
-    
+
     return authtoken;
-    
-  });
+
+});
--- dist/Channels/controllers/mychannelsCtrl.js
+++ dist/Channels/controllers/mychannelsCtrl.js
@@ -8,7 +8,10 @@ angular.module('IpsumFE.Channels').controller('mychannelsCtrl', function ($scope
                 "CreatedAt": "04/09/2014",
                 "Description": "ac tellus. Suspendisse sed dolor.",
                 "Detail": "nulla at sem molestie sodales. Mauris blandit enim consequat purus. Maecenas libero est, congue a, aliquet",
-                "Type": "Premium",
+                "Premium": false,
+                "Active": true,
+                "Visible": true,
+                "Contents": 34,
                 "id": "1"
             },
             {
@@ -16,7 +19,10 @@ angular.module('IpsumFE.Channels').controller('mychannelsCtrl', function ($scope
                 "CreatedAt": "10/06/2014",
                 "Description": "et magnis dis",
                 "Detail": "cursus in,",
-                "Type": "Free",
+                "Premium": true,
+                "Active": true,
+                "Visible": true,
+                "Contents": 4,
                 "id": "2"
             },
             {
@@ -24,7 +30,10 @@ angular.module('IpsumFE.Channels').controller('mychannelsCtrl', function ($scope
                 "CreatedAt": "16/05/2014",
                 "Description": "venenatis vel, faucibus",
                 "Detail": "amet, consectetuer adipiscing elit. Etiam laoreet, libero et tristique pellentesque, tellus sem mollis dui, in sodales elit erat vitae risus. Duis a mi fringilla mi lacinia mattis. Integer eu",
-                "Type": "Free",
+                "Premium": false,
+                "Active": true,
+                "Visible": false,
+                "Contents": 0,
                 "id": "3"
             },
             {
@@ -32,7 +41,10 @@ angular.module('IpsumFE.Channels').controller('mychannelsCtrl', function ($scope
                 "CreatedAt": "21/05/2015",
                 "Description": "Nunc ac sem",
                 "Detail": "ultrices, mauris ipsum porta elit, a feugiat tellus lorem eu metus. In lorem. Donec elementum, lorem",
-                "Type": "Premium",
+                "Premium": true,
+                "Active": false,
+                "Visible": false,
+                "Contents": 12,
                 "id": "4"
             },
             {
@@ -40,7 +52,10 @@ angular.module('IpsumFE.Channels').controller('mychannelsCtrl', function ($scope
                 "CreatedAt": "08/05/2014",
                 "Description": "non enim",
                 "Detail": "magnis dis parturient montes, nascetur ridiculus mus. Proin vel nisl. Quisque fringilla euismod enim. Etiam gravida molestie arcu. Sed eu nibh vulputate mauris sagittis placerat. Cras dictum ultricies ligula. Nullam",
-                "Type": "Premium",
+                "Premium": false,
+                "Active": true,
+                "Visible": true,
+                "Contents": 5,
                 "id": "5"
             }
      ];
